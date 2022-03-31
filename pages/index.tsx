7b0b505a29ac1b67ed7dc02c87b599cd98f60718
Richard Humel <darken141@gmail.com>
2022-03-31 12:53:01 +0200

    update meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -185,22 +185,31 @@ const Home: NextPage = () => {
 		<React.Fragment>
 			<Head>
 				{/* <!-- Primary Meta Tags --> */}
-				<title>amsterDOT Hack Conf</title>
-				<meta name="title" content="amsterDOT Hack Conf" />
-				<meta name="description" content="amsterDOT Hack Conf" />
+				<title>AmsterDOT ConfHack</title>
+				<meta name="title" content="AmsterDOT ConfHack" />
+				<meta
+					name="description"
+					content="4 days of conf & hack sessions organized by some of the leading projects of the polkadot and kusama ecosystems."
+				/>
 
 				{/* <!-- Open Graph / Facebook --> */}
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content="https://amsterdot.io/" />
-				<meta property="og:title" content="amsterDOT Hack Conf" />
-				<meta property="og:description" content="amsterDOT Hack Conf" />
+				<meta property="og:title" content="AmsterDOT ConfHack" />
+				<meta
+					property="og:description"
+					content="4 days of conf & hack sessions organized by some of the leading projects of the polkadot and kusama ecosystems."
+				/>
 				<meta property="og:image" content="/assets/meta.jpeg" />
 
 				{/* <!-- Twitter --> */}
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta property="twitter:url" content="https://amsterdot.io/" />
-				<meta property="twitter:title" content="amsterDOT Hack Conf" />
-				<meta property="twitter:description" content="amsterDOT Hack Conf" />
+				<meta property="twitter:title" content="AmsterDOT ConfHack" />
+				<meta
+					property="twitter:description"
+					content="4 days of conf & hack sessions organized by some of the leading projects of the polkadot and kusama ecosystems."
+				/>
 				<meta property="twitter:image" content="/assets/meta.jpeg" />
 			</Head>
 
