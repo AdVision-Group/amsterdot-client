8db699754d7fd128939791d238ecc4f6690848ad
Richard Humel <darken141@gmail.com>
2022-04-05 12:50:53 +0200

    remove console logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,7 +101,7 @@ const Home: NextPage = () => {
 	// 	[0, 1, 1, 1]
 	// )
 
-	scrollYProgress.onChange((e) => console.log(e))
+	// scrollYProgress.onChange((e) => console.log(e))
 
 	// Set horizontal container width on change
 	// useEffect(() => {
